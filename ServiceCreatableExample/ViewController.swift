8fcf58af347efaa0db8488252a30206cbee870af
keran.marinov <keran.fm@gmail.com>
2017-10-02 21:26:11 +0100

    Initial Commit

---
+++ ServiceCreatableExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServiceCreatableExample
+//
+//  Created by Linxmap on 02/10/2017.
+//  Copyright © 2017 linxmap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
